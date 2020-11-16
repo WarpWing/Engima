ee551aba5d2f604204699664be3142421a697a42
WarpWing <professornitro@gmail.com>
2020-11-16 02:17:16 +0000

    Added Clap to dependecies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("This is a test");
+use clap::App; 
+
+fn main() { 
+    App::new("mps")
+       .version("1.0")
+       .about("Does great things!")
+       .author("Kevin K.")
+       .get_matches(); 
 }
