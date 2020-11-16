a79077570c6ecca8e4943297dcf9a6b4b8374f4c
WarpWing <professornitro@gmail.com>
2020-11-16 22:50:53 +0000

    Reorganized some stuff

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import pretty_errors
 import os
 
 app = 'MultipassSimplified'
-#Multipass Install Check
 username = getpass.getuser()
 x = f'/home/{username}/snap/multipass'
 isFile = os.path.isdir(x) 
 
-if isFile == True: 
-    print(f"Multipass is installed! Loading up {app}")
-else: 
-    print(f"Please install Multipass before launching {app} ") 
+#Multipass Install Check
+def multipasscheck():
+    if isFile == True: 
+        print(f"Multipass is installed! Loading up {app}")
+    else: 
+        print(f"Please install Multipass before launching {app} ") 
+
+multipasscheck()
