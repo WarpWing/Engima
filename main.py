d914f1197ee322ac2ae6ad0a2d366b9bfa59ab46
WarpWing <professornitro@gmail.com>
2020-11-16 23:07:50 +0000

    Misc

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from __future__ import print_function, unicode_literals
+from PyInquirer import prompt
+from pprint import pprint
+import sys
+from sys import exit
 import getpass
 import PyInquirer
 import time
 import pretty_errors
 import os
-from __future__ import print_function, unicode_literals
-from PyInquirer import prompt
-from pprint import pprint
+
 
 app = 'MultipassSimplified'
 username = getpass.getuser()
@@ -15,12 +18,23 @@ isFile = os.path.isdir(x)
 #Multipass Install Check
 def multipasscheck():
     if isFile == True: 
-        print(f"Multipass is installed! Loading up {app}")
+        pass
     else: 
         print(f"Please install Multipass before launching {app} ") 
+        exit()
 
-os.system('clear')
+# Invoking Multipass Check
 multipasscheck()
-time.sleep(2)
+
+# Actual Console
 def console():
-    
+    questions = [
+    {
+        'type': 'input',
+        'name': 'first_name',
+        'message': 'What\'s your first name',
+     }
+    ]
+    answers = prompt(questions)
+    pprint(answers)
+console()
