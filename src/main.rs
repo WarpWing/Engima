c6b82d051af3d91dcabcfc4dc845ac8bc3a2fae8
WarpWing <professornitro@gmail.com>
2020-11-16 02:32:52 +0000

    Something

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ extern crate clap;
 use clap::App; 
 
 fn main() { 
+    println!("Hello, world!");
     App::new("mps")
        .version("v1.0")
        .about("A Rust Written TUI designed to help users navitage Canonical's Multipass VM System with ease.")
-       .author("Ty Chermsirivatana")
+       .author("Ty C.")
        .get_matches(); 
-}
+} 
