e0fb38a6afa6d795ae3fa77abcef07debb037ee1
WarpWing <professornitro@gmail.com>
2020-11-18 00:03:06 +0000

    Logicc

--- main.py
+++ main.py
@@ -17,7 +17,33 @@ def multipasscheck():
     else: 
         print(f"Please install Multipass before launching {app} ") 
         exit()
-
+# MPS Console Logic 
+def mainconsole(): 
+    g = input('Enter your input: ')
+    f = int(g)
+    if f == 1:
+        print("Launch Instance with Parameters")
+    elif f == 2: 
+        print("Stop Instance with Parameters")
+    elif f == 3:
+        print("Delete Instance with Parameters")
+    elif f == 4: 
+        pass
+    elif f == 5:
+        pass
+    elif f == 6:
+        pass
+    elif f == 7:
+        pass
+    elif f == 8:
+        pass
+    elif f == 9:   
+        pass  
+    else:
+        print("Invalid Operation!")
+     
+        
 # Invoking Multipass Check
-multipasscheck()
-
+multipasscheck() 
+# Invoke Console
+mainconsole()
