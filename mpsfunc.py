9a20ca3351702ce3161e854b76421a9ccfab81a3
WarpWing <professornitro@gmail.com>
2020-11-18 13:04:15 +0000

    Readded base framework from Sentinel Ace Backup

--- mpsfunc.py
+++ mpsfunc.py
@@ -1,1 +1,13 @@
+from consolemenu import *
+from consolemenu.items import *
+import sys
+import getpass
+import time
+import pretty_errors
+import os 
 # MPS Responsive Function Array
+
+# WarpWing Test Function
+def warpwing():
+    print("Function Test")
+    time.sleep(6)
