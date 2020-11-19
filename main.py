d763b5365034932f70e7890a7de28396eaa51e91
WarpWing <professornitro@gmail.com>
2020-11-19 00:20:49 +0000

    Finally made Multipass Launch Command

--- main.py
+++ main.py
@@ -1,39 +1,55 @@
+from colorama import Fore, Style
 from consolemenu import *
 from consolemenu.items import *
-from mpsfunc import * 
-import sys
-import getpass
-import time
 import pretty_errors
+import colorama
+import time
 import os
+import system
 
-# File Wide Variables
-app = 'MultipassSimplified'
-username = getpass.getuser()
-m = f'/home/{username}/snap/multipass'  # Linux
-w = f"c:/Program Files/multipass"       # Windows
-isFile = os.path.isdir(m) 
-
-#Multipass Install Check
-def multipassCheck():
-    if isFile: 
-        pass
-    else: 
-        print(f"Please install Multipass before launching {app} ") 
-        exit()
-
-
+print(Fore.GREEN)
 # GUI Logic 
 def mainConsole():
     # GUI Logic 
     menu = ConsoleMenu("MultipassSimplified", "A Python TUI Interface for the Multipass VM System by WarpWing")
     # Command Logic 
-    command_item1 = FunctionItem("Create New MP Instance", warpwing)
+    launchMP = FunctionItem("Create New MP Instance", launchMPI)
     # Menu Append Array
-    menu.append_item(command_item1)
+    menu.append_item(launchMP)
     # Menu Show Function 
     menu.show()     
-# Invoking Multipass Check
-multipassCheck() 
-# Invoke Console
+# Launch MP Logic 
+def launchMPI():
+    print("What would like to name this MP Instance?")
+    mpname = input("Name: ")
+    print("How many CPUs would you like to allocate?")
+    print("The minimum CPUs is 1 and the default CPU allocation is 1 if left blank")
+    mpcpu = input("CPUs: ")
+    print("How much Memory would you like to allocate?")
+    print("Please use the suffixes of K,M or G in declaring memory.") 
+    print("The minimum Memory is 128M and the default is 1G if left blank")
+    mpmem = input("Memory: ")
+    print("How much disk space would you like to allocate?")
+    print("Please use the suffixes of K,M or G in declaring disk space.")
+    print("The minimum storage is 512M and the default is 5G")
+    mpdisk = input("Space: ")
+    print(f"To finalize the creation of this MP Instance,please verify the following settings:")
+    print(f"Name: {mpname}")
+    print(f"CPUs: {mpcpu}")
+    print(f"Memory: {mpmem}")
+    print(f"Disk Space: {mpdisk}")
+    mpfinal = input("Is this correct? y/n?: ")
+    if mpfinal == "y":
+        print("Creating MP Instance with the following settings")
+        time.sleep(2)
+        os.system(f"multipass launch -n {mpname} -c {mpcpu} -m {mpmem} -d {mpdisk}")
+        print(f"Instance {mpname} created!")
+        time.sleep(3)
+    else: 
+        print("Going back to the main menu!") # I'll work on a fix later that allows you to either resubmit settings or go back to the main menu.
+        time.sleep(5)
+# Invoking Console
 mainConsole()
+
+
+
